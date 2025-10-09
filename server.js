27f774bb4a780a765ca71a3a5b22cf38122f4a2c
yolma15 <carmesmayol@gmail.com>
2025-10-09 08:40:03 +0200

    TP1 09/10

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-import express from 'express';
-import fs from 'fs';
-import path from 'path';
+import express from "express";
+import fs from "fs";
+import path from "path";
 
 const app = express();
+app.set("view engine", "ejs");
 
-app.get('/', function(req, res) {
-    res.sendFile("./index.html", { root: import.meta.dirname });
+app.get("/", function (req, res) {
+  res.render("index", { MAVARIABLE: "Hello World!" });
 });
 
-app.use((req,res)=>{
-    res.status(404).sendFile("./404.html", { root: import.meta.dirname });
+app.use((req, res) => {
+  res.status(404).render("404");
 });
 
 app.listen(3000);
